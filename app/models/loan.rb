05d2ebcc78f43f8ada5eecac446625457adbbd64
github username <github email address>
2020-08-22 17:25:32 -0500

    added validates presence of car to loan model

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,14 +1,8 @@
 class Loan < ApplicationRecord
     belongs_to :user, optional: true
     belongs_to :car, optional: true
-     accepts_nested_attributes_for :car
-     validates :category, presence: true
-     validates :amount, presence: true
-
-   #   def car_manufacturer=(attributes)
-   #      self.car = Car.find_or_create_by(attributes)
-   #    end
-   #    def car
-   #       self.car ? self.car.manufacturer : nil
-   #    end
+    validates_presence_of :car, {:message => "must have a customer name."}
+    accepts_nested_attributes_for :car
+    validates :category, presence: true
+    validates :amount, presence: true
 end
